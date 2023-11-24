2daa3683a915c16f4e4bbee9fcb2217f7925d934
realwanheda <vabhav.tripathi@s.amity.edu>
2023-11-24 06:41:54 +0530

    Input Fields

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,9 +1,56 @@
-import React from "react";
 import styles from "./Body.module.css";
 import { ArrowDown } from "react-feather";
+import Editor from "../Editor/Editor";
+import React, { useRef, useState } from "react";
 
 function Body() {
   const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed89e6"];
+  const sections = {
+    basicInfo: "Basic Info",
+    workExp: "Work Experience",
+    project: "Projects",
+    education: "Education",
+    achievements: "Achievement",
+    summary: "Summary",
+    other: "Other",
+  };
+  const [resumeInformation, setResumeInformation] = useState({
+    [sections.basicInfo]: {
+      id: sections.basicInfo,
+      sectionTitle: sections.basicInfo,
+      detail: {},
+    },
+    [sections.workExp]: {
+      id: sections.workExp,
+      sectionTitle: sections.workExp,
+      details: [],
+    },
+    [sections.project]: {
+      id: sections.project,
+      sectionTitle: sections.project,
+      details: [],
+    },
+    [sections.education]: {
+      id: sections.education,
+      sectionTitle: sections.education,
+      details: [],
+    },
+    [sections.achievement]: {
+      id: sections.achievement,
+      sectionTitle: sections.achievement,
+      points: [],
+    },
+    [sections.summary]: {
+      id: sections.summary,
+      sectionTitle: sections.summary,
+      detail: "",
+    },
+    [sections.other]: {
+      id: sections.other,
+      sectionTitle: sections.other,
+      detail: "",
+    },
+  });
   return (
     <div className={styles.container}>
       <p className={styles.heading}>Resume Builder </p>
@@ -21,6 +68,9 @@ function Body() {
           Download
           <ArrowDown />
         </button>
+      </div>
+      <div className={styles.main}>
+        <Editor sections={sections} information={resumeInformation} />
       </div>
     </div>
   );
--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,8 +1,243 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Editor.module.css";
+import InputControl from "../InputControl/InputControl";
+import { X } from "react-feather";
 
-function Editor() {
-  return <div></div>;
+function Editor(props) {
+  const sections = props.sections;
+  const information = props.information;
+  const [activeSectionKey, setActiveSectionkey] = useState(
+    Object.keys(sections)[0]
+  );
+  const [activeInformation, setActiveInformation] = useState(
+    information[sections[Object.keys(sections)[0]]]
+  );
+  const [sectionTitle, setSectionTitle] = useState(
+    sections[Object.keys(sections)[0]]
+  );
+  const [values, setValues] = useState({
+    name: activeInformation?.detail?.name || "",
+    title: activeInformation?.detail?.title || "",
+    linkedin: activeInformation?.detail?.linkedin || "",
+    github: activeInformation?.detail?.github || "",
+    phone: activeInformation?.detail?.phone || "",
+    email: activeInformation?.detail?.email || "",
+  });
+
+  const workExpBody = (
+    <div className={styles.detail}>
+      <div className={styles.row}>
+        <InputControl
+          label="Title"
+          placeholder="Enter title eg. Frontend developer"
+          defaultValue={values.title}
+        />
+        <InputControl
+          label="Company Name"
+          placeholder="Enter company name eg. amazon"
+        />
+      </div>
+      <div className={styles.row}>
+        <InputControl
+          label="Certificate Link"
+          placeholder="Enter certificate link"
+        />
+        <InputControl
+          label="Location"
+          placeholder="Enter location eg. Remote"
+        />
+      </div>
+      <div className={styles.row}>
+        <InputControl
+          label="Start Date"
+          type="date"
+          placeholder="Enter start date of work"
+        />
+        <InputControl
+          label="End Date"
+          type="date"
+          placeholder="Enter end date of work"
+        />
+      </div>
+      <div className={styles.column}>
+        <label>Enter Work Description</label>
+        <InputControl placeholder="Line1" />
+        <InputControl placeholder="Line3" />
+        <InputControl placeholder="Line3" />
+      </div>
+    </div>
+  );
+
+  const projectBody = (
+    <div className={styles.detail}>
+      <div className={styles.row}>
+        <InputControl label="Title" placeholder="Enter title eg. Chat app" />
+      </div>
+      <InputControl
+        label="Overview"
+        placeholder="Enter basic overview of project"
+      />
+      <div className={styles.row}>
+        <InputControl
+          label="Deployed Link"
+          placeholder="Enter deployed link of project"
+        />
+        <InputControl
+          label="Github Link"
+          placeholder="Enter github link of project"
+        />
+      </div>
+      <div className={styles.column}>
+        <label>Enter project description</label>
+        <InputControl placeholder="Line 1" />
+        <InputControl placeholder="Line 2" />
+        <InputControl placeholder="Line 3" />
+        <InputControl placeholder="Line 4" />
+      </div>
+    </div>
+  );
+  const educationBody = (
+    <div className={styles.detail}>
+      <div className={styles.row}>
+        <InputControl label="Title" placeholder="Enter title eg. B-tech" />
+      </div>
+      <InputControl
+        label="College/School Name"
+        placeholder="Enter name of your college/school"
+      />
+      <div className={styles.row}>
+        <InputControl
+          label="Start Date"
+          type="date"
+          placeholder="Enter start date of this education"
+        />
+        <InputControl
+          label="End Date"
+          type="date"
+          placeholder="Enter end date of this education"
+        />
+      </div>
+    </div>
+  );
+  const basicInfoBody = (
+    <div className={styles.detail}>
+      <div className={styles.row}>
+        <InputControl
+          label="Name"
+          placeholder="Enter your full name eg. Aashu"
+        />
+        <InputControl
+          label="Title"
+          placeholder="Enter your title eg. Frontend developer"
+        />
+      </div>
+      <div className={styles.row}>
+        <InputControl
+          label="Linkedin Link"
+          placeholder="Enter your linkedin profile link"
+        />
+        <InputControl
+          label="Github Link"
+          placeholder="Enter your github profile link"
+        />
+      </div>
+      <div className={styles.row}>
+        <InputControl label="Email" placeholder="Enter your email" />
+        <InputControl
+          label="Enter phone"
+          placeholder="Enter your phone number"
+        />
+      </div>
+    </div>
+  );
+  const achievementsBody = (
+    <div className={styles.detail}>
+      <div className={styles.column}>
+        <label>List your achievements</label>
+        <InputControl placeholder="Line 1" />
+        <InputControl placeholder="Line 2" />
+        <InputControl placeholder="Line 3" />
+        <InputControl placeholder="Line 4" />
+      </div>
+    </div>
+  );
+  const summaryBody = (
+    <div className={styles.detail}>
+      <InputControl
+        label="Summary"
+        placeholder="Enter your objective/summary"
+      />
+    </div>
+  );
+  const otherBody = (
+    <div className={styles.detail}>
+      <InputControl label="Other" placeholder="Enter something" />
+    </div>
+  );
+  const generateBody = () => {
+    switch (sections[activeSectionKey]) {
+      case sections.basicInfo:
+        return basicInfoBody;
+      case sections.workExp:
+        return workExpBody;
+      case sections.project:
+        return projectBody;
+      case sections.education:
+        return educationBody;
+      case sections.achievements:
+        return achievementsBody;
+      case sections.summary:
+        return summaryBody;
+      case sections.other:
+        return otherBody;
+      default:
+        return null;
+    }
+  };
+  useEffect(() => {
+    setActiveInformation(information[sections[activeSectionKey]]);
+    setSectionTitle(sections[activeSectionKey]);
+  }, [activeSectionKey]);
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        {Object.keys(sections)?.map((key) => (
+          <div
+            className={`${styles.section} ${
+              activeSectionKey === key ? styles.active : ""
+            }`}
+            key={key}
+            onClick={() => setActiveSectionkey(key)}
+          >
+            {sections[key]}
+          </div>
+        ))}
+      </div>
+      <div className={styles.body}>
+        <InputControl
+          label="Title"
+          placeholder="Enter Section Title"
+          value={sectionTitle}
+          onChange={(event) => setSectionTitle(event.target.value)}
+        />
+
+        <div className={styles.chips}>
+          {activeInformation?.details
+            ? activeInformation?.details?.map((item, index) => (
+                <div className={styles.chip} key={item.title + index}>
+                  <p>
+                    {sections[activeSectionKey]} {index + 1}
+                  </p>
+                  <X />
+                </div>
+              ))
+            : ""}
+        </div>
+        {generateBody()}
+        <button>Save</button>
+      </div>
+    </div>
+  );
 }
 
 export default Editor;
