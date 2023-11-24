4f65b38ac5bbabc7efe1de48976e97435682d3e2
realwanheda <vabhav.tripathi@s.amity.edu>
2023-11-25 00:29:58 +0530

    Done

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,19 +1,26 @@
-import styles from "./Body.module.css";
-import { ArrowDown } from "react-feather";
-import Editor from "../Editor/Editor";
 import React, { useRef, useState } from "react";
+import ReactToPrint from "react-to-print";
+import { ArrowDown } from "react-feather";
+
+import Editor from "../Editor/Editor";
+import Resume from "../Resume/Resume";
+
+import styles from "./Body.module.css";
 
 function Body() {
-  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed89e6"];
+  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
   const sections = {
     basicInfo: "Basic Info",
     workExp: "Work Experience",
     project: "Projects",
     education: "Education",
-    achievements: "Achievement",
+    achievement: "Achievements",
     summary: "Summary",
     other: "Other",
   };
+  const resumeRef = useRef();
+
+  const [activeColor, setActiveColor] = useState(colors[0]);
   const [resumeInformation, setResumeInformation] = useState({
     [sections.basicInfo]: {
       id: sections.basicInfo,
@@ -51,26 +58,46 @@ function Body() {
       detail: "",
     },
   });
+
   return (
     <div className={styles.container}>
-      <p className={styles.heading}>Resume Builder </p>
+      <p className={styles.heading}>Resume Builder</p>
       <div className={styles.toolbar}>
         <div className={styles.colors}>
           {colors.map((item) => (
             <span
               key={item}
               style={{ backgroundColor: item }}
-              className={styles.color}
+              className={`${styles.color} ${
+                activeColor === item ? styles.active : ""
+              }`}
+              onClick={() => setActiveColor(item)}
             />
           ))}
         </div>
-        <button>
-          Download
-          <ArrowDown />
-        </button>
+        <ReactToPrint
+          trigger={() => {
+            return (
+              <button>
+                Download <ArrowDown />
+              </button>
+            );
+          }}
+          content={() => resumeRef.current}
+        />
       </div>
       <div className={styles.main}>
-        <Editor sections={sections} information={resumeInformation} />
+        <Editor
+          sections={sections}
+          information={resumeInformation}
+          setInformation={setResumeInformation}
+        />
+        <Resume
+          ref={resumeRef}
+          sections={sections}
+          information={resumeInformation}
+          activeColor={activeColor}
+        />
       </div>
     </div>
   );
